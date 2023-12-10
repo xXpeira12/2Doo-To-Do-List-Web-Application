ad25027b9d7f14ba4f97b1f3c7caf3aefaed292d
xXpeira12 <78487961+xXpeira12@users.noreply.github.com>
2023-12-10 16:51:11 +0700

    Initial commit for GitHub Pages

---
+++ config.js
@@ -0,0 +1,21 @@
+/**
+ * @typedef {Object} Item
+ * @property {string} _id
+ * @property {string} name
+ * @property {date} endTime
+ * @property {date} startTime
+ * @property {string} day
+ */
+
+/** @typedef {Omit<Item, "_id">} ItemPayload */
+
+// เปลี่ยน localhost -> Public IPv4 address
+export const BACKEND_URL = "http://localhost:3222";
+
+// export const MEMBERS = [
+//   "ติณณา เจียรวาปี",
+//   "ปภาวิน ตั้งจิตร์พร",
+//   "เพ็ญพิชชา อยู่หุ่น",
+//   "ศิรสิทธิ์ ธนาจิรวัฒน์",
+//   "อัมพิกาภา ทาทอง"
+// ];
