c2fc4dffa1c118083be98b89dd30db8afd0eca62
Sirasit Tanajirawat <78487961+xXpeira12@users.noreply.github.com>
2023-12-11 12:06:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,11 +12,3 @@
 // เปลี่ยน localhost -> Public IPv4 address
 // export const BACKEND_URL = "http://localhost:3222";
 export const BACKEND_URL = "https://backend-service-kdww.onrender.com";
-
-// export const MEMBERS = [
-//   "ติณณา เจียรวาปี",
-//   "ปภาวิน ตั้งจิตร์พร",
-//   "เพ็ญพิชชา อยู่หุ่น",
-//   "ศิรสิทธิ์ ธนาจิรวัฒน์",
-//   "อัมพิกาภา ทาทอง"
-// ];
